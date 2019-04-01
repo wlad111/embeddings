b26f5d62a266b9e0d844f77acc9169104682bfd7
wlad111 <wlad962961@gmail.com>
2019-04-01 19:27:04 +0300

    initial

---
+++ library.cpp
@@ -0,0 +1,8 @@
+#include "library.h"
+#include "extern/ml_lib/cpp/core/vec.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef EMBEDDINGS_LIBRARY_H
+#define EMBEDDINGS_LIBRARY_H
+
+void hello();
+
+#endif //EMBEDDINGS_LIBRARY_H
